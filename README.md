9a30d414dbf5f4d6a0a32b637084f02a53e3b9dc
wdamien <wes@gskinner.com>
2013-06-25 14:05:41 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Combined
-Hosts the combined CreateJS library, and the tools for building it.
+## We use grunt (http://gruntjs.com/) to manage our build process.
+
+## To use
+
+### Install dependencies
+
+Node (0.10.x or greater is required):
+
+   # check the version
+   node -v
+
+If your node is out of date, install the latest from:
+http://nodejs.org/
+
+After node is setup, install the other dependances:
+
+   # Install the grunt command line utility
+   sudo npm install grunt-cli -g
+
+   # Install all the dependencies for this project.
+   npm install
+
+### Setup
+
+You'll need to change the default settings to suit your work environment.
+We have 2 config files:
+
+* config.json - Is meant to be in git and pushed to all developers.
+* config.local.json - Is added to .gitignore and and only for your local setup (any settings in here will override those in config.json)
+
+Please adjust these settings to match your environment. All paths can either be relative from this folder, or absolute paths.
+
+* easel_path
+* preload_path
+* sound_path
+* tween_path
+
+### Building
+To export a release build for this library run:
+
+   grunt build
+
+This command will:
+
+* Update Easels and MovieClips version files.
+* Create the easeljs-{VERSION}.min.js file
+* Create a easeljs-{VERSION}.combined.js file
+* Compile the docs and create a zip of them
+* Create the movieclip-{VERSION}.min.js
+* Copy the docs zip to ../docs
+* Copy the built js file to ../lib
+
+To build the NEXT version run:
+
+grunt next
+
+Does the exact same process as above but uses NEXT as the version.
+
+### Combined build
+You can optionally create the combined and minified file of all the libraries.
+
+   grunt all
+
+Or to build a NEXT version
+
+	grunt all-next
+
+
+### All commands
+
+grunt build -  Build everything based on the version in package.json
+grunt next - Build everything using the NEXT version.
+grunt docs - Build only the docs
+grunt uglify - Create the Easel and MovieClip min files. (Will use NEXT as the version)
---
+++ gruntfile.js
@@ -0,0 +1,119 @@
+var path = require('path');
+
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Setup Uglify for JS minification.
+		uglify: {
+			options: {
+				banner: grunt.file.read('LICENSE'),
+				preserveComments: "some",
+				compress: {
+					global_defs: {
+						"DEBUG": false
+					}
+				},
+			},
+			build:  {
+				files: {
+					'createjs-<%= grunt.template.today("yyyy.mm.dd") %>.min.js':getCombinedSource()
+				}
+			}
+		} ,
+		hub: {
+			build: {
+				src: getHubTasks(),
+				tasks: ['build'],
+			},
+			next: {
+				src: getHubTasks(),
+				tasks: ['next'],
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-hub');
+
+	grunt.registerTask('build', ['hub:build']);
+	grunt.registerTask('next', ['hub:next']);
+
+	function getBuildConfig() {
+		var config = grunt.config('config');
+		if (config) {
+			return config;
+		}
+
+		// Read the global settings file first.
+		var config = grunt.file.readJSON('config.json');
+
+		// If we have a config.local.json .. prefer its values.
+		if (grunt.file.exists('config.local.json')) {
+			var config2 = grunt.file.readJSON('config.local.json');
+			_.extend(config, config2);
+		}
+
+		grunt.config.set('config', config);
+
+		return config;
+	}
+
+	function getConfigValue(name) {
+		var config = getBuildConfig();
+
+		if (config == null) {
+			config = grunt.file.readJSON('config.local.json');
+			grunt.config.set('config', config);
+		}
+
+		return config[name];
+	}
+
+	function getHubTasks() {
+		var files = [
+			getConfigValue('easel_path')+'build/Gruntfile.js',
+			getConfigValue('preload_path')+'build/Gruntfile.js',
+			getConfigValue('sound_path')+'build/Gruntfile.js',
+			getConfigValue('tween_path')+'build/Gruntfile.js'
+		];
+		return files;
+	}
+
+	function getCombinedSource() {
+		// Pull in all the config files (order matters here!)
+		var configs = [
+			{cwd: getConfigValue('easel_path') + '/build/', config:'config.json', source:'easel_source'},
+			{cwd: getConfigValue('preload_path') + '/build/', config:'config.json', source:'source'},
+			{cwd: getConfigValue('sound_path') + '/build/', config:'config.json', source:'source'},
+			{cwd: getConfigValue('tween_path') + '/build/', config:'config.json', source:'source'},
+		]
+
+		// Pull out all the source paths.
+		var sourcePaths = [];
+		for (var i=0;i<configs.length;i++) {
+			var o = configs[i];
+			var json = grunt.file.readJSON(path.resolve(o.cwd, o.config));
+			var sources = json[o.source];
+			sources.forEach(function(item, index, array) {
+				array[index] = path.resolve(o.cwd, item);
+			});
+			sourcePaths = sourcePaths.concat(sources);
+		}
+
+		// Remove duplicates (Like EventDispatcher)
+		var dups = {};
+		var clean = [];
+		for (i=0;i<sourcePaths.length;i++) {
+			var src = sourcePaths[i];
+			var cleanSrc = src.substr(src.lastIndexOf('src/'));
+			if  (dups[cleanSrc] == null) {
+				clean.push(src);
+				dups[cleanSrc] = true;
+			}
+		}
+
+		return clean;
+	}
+}
